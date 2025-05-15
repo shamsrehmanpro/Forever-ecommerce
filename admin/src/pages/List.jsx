bb033abb537911c1da5b5c71375019bb31ddbe1e
shamsrehmanpro <shamsrehmanpro@gmail.com>
2025-05-15 16:40:29 +0500

    showing list

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { backendUrl } from '../App'
+import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify'
 
 const List = () => {
@@ -21,9 +21,33 @@ const List = () => {
   },[])
 
   return (
-    <div>
+    <>
+     <p className='mb-2'>All Products List</p>
+     <div className='flex flex-col gap-2 '>
       
-    </div>
+      {/* list table title */}
+
+      <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
+        <b>Image</b>
+        <b>Name</b>
+        <b>Category</b>
+        <b>Price</b>
+        <b className='text-center'>Action</b>
+      </div>
+
+      {/* product list */}
+
+      {list.map((item, index)=>(
+        <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
+          <img className='w-12' src={item.image[0]} alt="" />
+          <p>{item.name}</p>
+          <p>{item.category}</p>
+          <p>{currency}{item.price}</p>
+          <p className='text-right md:text-center cursor-pointer text-lg '>X</p>
+        </div>
+      ))}
+      </div> 
+    </>
   )
 }
 
